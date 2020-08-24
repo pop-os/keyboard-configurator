ae2027866efd50769909f32473a36809cf41a048
Ian Douglas Scott <idscott@system76.com>
2020-08-24 15:40:13 -0700

    feat(windows): Build and run on Windows
    Doesn't actually set anything (yet).

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,18 @@ mod color_circle;
 mod color_wheel;
 mod keyboard_color_button;
 
+#[cfg(target_os = "linux")]
 use system76_power::{client::PowerClient, Power};
 
 pub use keyboard_color_button::KeyboardColorButton;
 
+#[cfg(target_os = "linux")]
 fn set_keyboard_color(rgb: color::Rgb) {
     let mut client = PowerClient::new().unwrap();
     client.set_keyboard_color(&rgb.to_string()).unwrap();
+}
+
+#[cfg(windows)]
+fn set_keyboard_color(_rgb: color::Rgb) {
+    eprintln!("Color setting not implemented on Windows yet");
 }
