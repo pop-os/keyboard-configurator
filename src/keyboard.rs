8eed2c7f2e3557b81a1d97031d63e6794b65b406
Ian Douglas Scott <idscott@system76.com>
2020-09-01 15:33:33 -0700

    improv: Migrate to new keyboard API in system76-power

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -3,12 +3,17 @@ use gio::prelude::*;
 use glib::clone;
 use glib::clone::{Downgrade, Upgrade};
 use glib::translate::{from_glib_none, ToGlibPtr};
-use std::cell::{Cell, RefCell};
+use glib::variant::Variant;
+use std::cell::RefCell;
+use std::collections::HashMap;
 use std::fmt;
 use std::iter::Iterator;
 use std::rc::{Rc, Weak};
 
 use crate::color::Rgb;
+
+const DBUS_NAME: &'static str = "com.system76.PowerDaemon";
+const DBUS_KEYBOARD_IFACE: &'static str = "com.system76.PowerDaemon.Keyboard";
 
 pub enum KeyboardPattern {
     Solid,
@@ -20,8 +25,13 @@ pub enum KeyboardPattern {
 
 enum KeyboardImplementation {
     #[cfg(target_os = "linux")]
-    S76Power(gio::DBusProxy),
-    Dummy(Cell<Rgb>, Cell<i32>),
+    S76Power {
+        proxy: gio::DBusProxy,
+        properties_proxy: gio::DBusProxy,
+    },
+    Dummy {
+        properties: HashMap<&'static str, RefCell<Variant>>,
+    },
 }
 
 struct KeyboardInner {
@@ -50,6 +60,43 @@ impl Upgrade for KeyboardWeak {
     }
 }
 
+// https://github.com/gtk-rs/glib/pull/651
+unsafe fn variant_get_child_value(variant: &glib::Variant, index: usize) -> glib::Variant {
+    from_glib_none(glib_sys::g_variant_get_child_value(
+        variant.to_glib_none().0,
+        index,
+    ))
+}
+
+fn set_property(
+    properties_proxy: &gio::DBusProxy,
+    property: &str,
+    value: glib::Variant,
+) -> Result<()> {
+    let variant: glib::Variant =
+        unsafe { from_glib_none(glib_sys::g_variant_new_variant(value.to_glib_none().0)) };
+    let args: glib::Variant = unsafe {
+        from_glib_none(glib_sys::g_variant_new_tuple(
+            vec![
+                DBUS_KEYBOARD_IFACE.to_variant(),
+                property.to_variant(),
+                variant,
+            ]
+            .to_glib_none()
+            .0,
+            3,
+        ))
+    };
+    properties_proxy.call_sync::<gio::Cancellable>(
+        "Set",
+        Some(&args),
+        gio::DBusCallFlags::NONE,
+        60000,
+        None,
+    )?;
+    Ok(())
+}
+
 impl Keyboard {
     fn new(implementation: KeyboardImplementation) -> Self {
         Self(Rc::new(KeyboardInner {
@@ -59,28 +106,43 @@ impl Keyboard {
     }
 
     #[cfg(target_os = "linux")]
-    fn new_s76Power() -> Self {
+    fn new_s76Power(path: &str) -> Self {
         // XXX unwrap
         let proxy = gio::DBusProxy::new_for_bus_sync::<gio::Cancellable>(
             gio::BusType::System,
             gio::DBusProxyFlags::NONE,
             None,
-            "org.freedesktop.UPower",
-            "/org/freedesktop/UPower/KbdBacklight",
-            "org.freedesktop.UPower.KbdBacklight",
+            DBUS_NAME,
+            path,
+            DBUS_KEYBOARD_IFACE,
             None,
         )
         .unwrap();
-        let keyboard = Self::new(KeyboardImplementation::S76Power(proxy));
+        let properties_proxy = gio::DBusProxy::new_for_bus_sync::<gio::Cancellable>(
+            gio::BusType::System,
+            gio::DBusProxyFlags::NONE,
+            None,
+            DBUS_NAME,
+            path,
+            "org.freedesktop.DBus.Properties",
+            None,
+        )
+        .unwrap();
+        let keyboard = Self::new(KeyboardImplementation::S76Power {
+            proxy,
+            properties_proxy,
+        });
         keyboard.connect_signals();
         keyboard
     }
 
     fn new_dummy() -> Self {
-        Self::new(KeyboardImplementation::Dummy(
-            Cell::new(Rgb::new(0, 0, 0)),
-            Cell::new(0),
-        ))
+        let mut properties = HashMap::new();
+        properties.insert("max_brightness", RefCell::new(100i32.to_variant()));
+        properties.insert("brightness", RefCell::new(0i32.to_variant()));
+        properties.insert("color", RefCell::new("ff0000".to_variant()));
+        properties.insert("name", RefCell::new("Dummy Keyboard".to_variant()));
+        Self::new(KeyboardImplementation::Dummy { properties })
     }
 
     fn connect_signals(&self) {
@@ -88,27 +150,50 @@ impl Keyboard {
 
         match &self.0.implementation {
             #[cfg(target_os = "linux")]
-            KeyboardImplementation::S76Power(proxy) => {
+            KeyboardImplementation::S76Power { proxy, .. } => {
                 proxy
                     .connect_local(
-                        "g-signal",
+                        "g-properties-changed",
                         true,
                         clone!(@weak self_ => @default-panic, move |args| {
-                            let signal_name: &str = args[2].get().unwrap().unwrap();
-                            let parameters: glib::Variant = args[3].get().unwrap().unwrap();
-                            if (signal_name == "BrightnessChanged") {
-                                let brightness: glib::Variant = unsafe {
-                                    from_glib_none(glib_sys::g_variant_get_child_value(parameters.to_glib_none().0, 0))
-                                };
-                                self_.brightness_changed(brightness.get().unwrap());
+                            let changed = args[1].get::<glib::Variant>().unwrap().unwrap().get::<glib::VariantDict>().unwrap();
+                            if let Some(brightness) = changed.lookup_value("brightness", None) {
+                                self_.brightness_changed(brightness.get::<i32>().unwrap());
                             }
                             None
                         }),
                     )
                     .unwrap();
             }
-            KeyboardImplementation::Dummy(_, _) => {}
+            KeyboardImplementation::Dummy { .. } => {}
         }
+    }
+
+    fn prop(&self, name: &'static str) -> Result<Option<Variant>> {
+        match &self.0.implementation {
+            #[cfg(target_os = "linux")]
+            KeyboardImplementation::S76Power { ref proxy, .. } => {
+                Ok(proxy.get_cached_property(name))
+            }
+            KeyboardImplementation::Dummy { properties } => {
+                Ok(Some(properties.get(name).unwrap().borrow().clone()))
+            }
+        }
+    }
+
+    pub fn set_prop(&self, name: &'static str, value: Variant) -> Result<()> {
+        match &self.0.implementation {
+            #[cfg(target_os = "linux")]
+            KeyboardImplementation::S76Power {
+                properties_proxy, ..
+            } => {
+                set_property(properties_proxy, name, value)?;
+            }
+            KeyboardImplementation::Dummy { properties } => {
+                *properties.get(name).unwrap().borrow_mut() = value;
+            }
+        }
+        Ok(())
     }
 
     /// Returns `true` if the keyboard has a backlight capable of setting color
@@ -118,32 +203,16 @@ impl Keyboard {
 
     /// Gets backlight color
     pub fn color(&self) -> Result<Rgb> {
-        match self.0.implementation {
-            #[cfg(target_os = "linux")]
-            KeyboardImplementation::S76Power(_) => {
-                use system76_power::{client::PowerClient, Power};
-                let mut client = PowerClient::new().map_err(Error::msg)?;
-                let color_str = client.get_keyboard_color().map_err(Error::msg)?;
-                Rgb::parse(&color_str).ok_or(Error::msg("Invalid color string"))
-            }
-            KeyboardImplementation::Dummy(ref c, _) => Ok(c.get()),
-        }
+        let color = match self.prop("color")? {
+            Some(value) => value.get().unwrap(),
+            None => "".to_string(),
+        };
+        Ok(Rgb::parse(&color).unwrap_or(Rgb::new(0, 0, 0)))
     }
 
     /// Sets backlight color
     pub fn set_color(&self, color: Rgb) -> Result<()> {
-        match &self.0.implementation {
-            #[cfg(target_os = "linux")]
-            KeyboardImplementation::S76Power(_) => {
-                use system76_power::{client::PowerClient, Power};
-                let mut client = PowerClient::new().map_err(Error::msg)?;
-                client
-                    .set_keyboard_color(&color.to_string())
-                    .map_err(Error::msg)?;
-            }
-            KeyboardImplementation::Dummy(ref c, _) => c.set(color),
-        }
-        Ok(())
+        self.set_prop("color", color.to_string().to_variant())
     }
 
     /// Returns `true` if the keyboard has a backlight capable of setting brightness
@@ -153,68 +222,23 @@ impl Keyboard {
 
     /// Gets backlight brightness
     pub fn brightness(&self) -> Result<i32> {
-        match &self.0.implementation {
-            #[cfg(target_os = "linux")]
-            KeyboardImplementation::S76Power(ref proxy) => {
-                let ret = proxy.call_sync::<gio::Cancellable>(
-                    "GetBrightness",
-                    None,
-                    gio::DBusCallFlags::NONE,
-                    60000,
-                    None,
-                )?;
-                let brightness: glib::Variant = unsafe {
-                    from_glib_none(glib_sys::g_variant_get_child_value(ret.to_glib_none().0, 0))
-                };
-                Ok(brightness.get().unwrap())
-            }
-            KeyboardImplementation::Dummy(_, b) => Ok(b.get()),
-        }
+        Ok(self
+            .prop("brightness")?
+            .map(|v| v.get().unwrap())
+            .unwrap_or(0))
     }
 
     /// Sets backlight brightness
     pub fn set_brightness(&self, brightness: i32) -> Result<()> {
-        match &self.0.implementation {
-            #[cfg(target_os = "linux")]
-            KeyboardImplementation::S76Power(ref proxy) => {
-                let args: glib::Variant = unsafe {
-                    from_glib_none(glib_sys::g_variant_new_tuple(
-                        vec![brightness.to_variant()].to_glib_none().0,
-                        1,
-                    ))
-                };
-                let brightness = proxy.call_sync::<gio::Cancellable>(
-                    "SetBrightness",
-                    Some(&args),
-                    gio::DBusCallFlags::NONE,
-                    60000,
-                    None,
-                )?;
-            }
-            KeyboardImplementation::Dummy(_, b) => b.set(brightness),
-        }
-        Ok(())
+        self.set_prop("brightness", brightness.to_variant())
     }
 
     /// Gets maximum brightness that can be set
     pub fn max_brightness(&self) -> Result<i32> {
-        match &self.0.implementation {
-            #[cfg(target_os = "linux")]
-            KeyboardImplementation::S76Power(ref proxy) => {
-                let ret = proxy.call_sync::<gio::Cancellable>(
-                    "GetMaxBrightness",
-                    None,
-                    gio::DBusCallFlags::NONE,
-                    60000,
-                    None,
-                )?;
-                let brightness: glib::Variant = unsafe {
-                    from_glib_none(glib_sys::g_variant_get_child_value(ret.to_glib_none().0, 0))
-                };
-                Ok(brightness.get().unwrap())
-            }
-            KeyboardImplementation::Dummy(_, _) => Ok(100),
-        }
+        Ok(self
+            .prop("max_brightness")?
+            .map(|v| v.get().unwrap())
+            .unwrap_or(100))
     }
 
     fn brightness_changed(&self, brightness: i32) {
@@ -250,17 +274,59 @@ impl Keyboard {
 
 impl fmt::Display for Keyboard {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match &self.0.implementation {
-            #[cfg(target_os = "linux")]
-            KeyboardImplementation::S76Power(_) => write!(f, "system76-power Keyboard"),
-            KeyboardImplementation::Dummy(_, _) => write!(f, "Dummy Keyboard"),
+        if let Ok(Some(value)) = self.prop("name") {
+            write!(f, "{}", value.get::<String>().unwrap())
+        } else {
+            write!(f, "")
         }
     }
 }
 
 #[cfg(target_os = "linux")]
 pub fn keyboards() -> impl Iterator<Item = Keyboard> {
-    vec![Keyboard::new_s76Power(), Keyboard::new_dummy()].into_iter()
+    // XXX unwrap
+    let proxy = gio::DBusProxy::new_for_bus_sync::<gio::Cancellable>(
+        gio::BusType::System,
+        gio::DBusProxyFlags::NONE,
+        None,
+        DBUS_NAME,
+        "/com/system76/PowerDaemon",
+        "org.freedesktop.DBus.ObjectManager",
+        None,
+    )
+    .unwrap();
+    let ret = proxy
+        .call_sync::<gio::Cancellable>(
+            "GetManagedObjects",
+            None,
+            gio::DBusCallFlags::NONE,
+            60000,
+            None,
+        )
+        .unwrap();
+
+    let mut keyboards = Vec::new();
+
+    let dict = unsafe { variant_get_child_value(&ret, 0) };
+    let iter = unsafe { glib_sys::g_variant_iter_new(dict.to_glib_none().0) };
+    loop {
+        let i = unsafe { glib_sys::g_variant_iter_next_value(iter) };
+        if i.is_null() {
+            break;
+        }
+        let i: glib::Variant = unsafe { from_glib_none(i) };
+        let path = unsafe { variant_get_child_value(&i, 0) }
+            .get::<String>()
+            .unwrap();
+        if path.starts_with("/com/system76/PowerDaemon/keyboard") {
+            keyboards.push(Keyboard::new_s76Power(&path));
+        }
+    }
+    unsafe { glib_sys::g_variant_iter_free(iter) };
+
+    keyboards.push(Keyboard::new_dummy());
+
+    keyboards.into_iter()
 }
 
 #[cfg(any(windows, target_os = "macos"))]
