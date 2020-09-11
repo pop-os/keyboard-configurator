c670b2845846c9fca6731e246747c7f533e5ecd7
Ian Douglas Scott <idscott@system76.com>
2020-09-11 15:04:06 -0700

    ci: Updates to macOS packaging
    .icns icon, change paths, verify plist

--- macos/package.sh
+++ macos/package.sh
@@ -5,14 +5,17 @@ set -e
 APP=System76KeyboardConfigurator.app
 EXECUTABLE=$APP/Contents/MacOS/keyboard_configurator
 
-mkdir -p $APP/Contents/{MacOS,Resources/lib}
+mkdir -p $APP/Contents/{MacOS/lib,Resources}
 
 # TODO: Release
 cp ../target/debug/examples/keyboard_color $EXECUTABLE
 
 for i in $(otool -L $EXECUTABLE | grep '/usr/local/opt/' | awk '{print $1}')
 do
-  cp $i $APP/Contents/Resources/lib
+  cp $i $APP/Contents/MacOS/lib
 done
+
+convert -background '#564e48' -fill white -size 256x256 -gravity center 'label:Keyboard\nConfigurator' keyboard-configurator.png
+makeicns -256 keyboard-configurator.png -out $APP/Contents/Resources/keyboard-configurator.icns
 
 appdmg appdmg.json keyboard-configurator.dmg
