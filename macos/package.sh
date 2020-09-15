7d5cf3d0310f1101151af594699d2cb6a14115e5
Ian Douglas Scott <idscott@system76.com>
2020-09-15 12:12:16 -0700

    improv(macos): Use gtk-mac-bundler to build app bundle
    Hopefully this will be more reliable than doing it manually.

--- macos/package.sh
+++ macos/package.sh
@@ -2,21 +2,12 @@
 
 set -e
 
-APP=System76KeyboardConfigurator.app
-EXECUTABLE=$APP/Contents/MacOS/keyboard_configurator
-
-mkdir -p $APP/Contents/{MacOS/lib,Resources}
-
-# TODO: Release
-cp ../target/debug/examples/keyboard_color $EXECUTABLE
-
-for i in $(otool -L $EXECUTABLE | grep '/usr/local/opt/' | awk '{print $1}')
-do
-  cp $i $APP/Contents/MacOS/lib
-done
-
 convert -background '#564e48' -fill white -size 256x256 -gravity center 'label:Keyboard\nConfigurator' keyboard-configurator.png
 makeicns -256 keyboard-configurator.png -out keyboard-configurator.icns
-cp keyboard-configurator.icns $APP/Contents/Resources/
+
+# TODO: Release
+cp ../target/debug/examples/keyboard_color keyboard-configurator
+
+gtk-mac-bundler keyboard-configurator.bundle
 
 appdmg appdmg.json keyboard-configurator.dmg
