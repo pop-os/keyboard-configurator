9d0387406f02809953eae66f3aa3a24f7e0a0151
Ian Douglas Scott <idscott@system76.com>
2021-02-22 07:12:28 -0800

    improv: Factor out redundant code into a `board()` in `server.rs`

--- src/daemon/server.rs
+++ src/daemon/server.rs
@@ -3,7 +3,7 @@ use ectool::AccessLpcLinux;
 use ectool::{Access, AccessHid, Ec};
 use hidapi::HidApi;
 use std::{
-    cell::{Cell, RefCell},
+    cell::{Cell, RefCell, RefMut},
     io::{self, BufRead, BufReader, Read, Write},
     str,
     time::Duration,
@@ -16,9 +16,9 @@ pub struct DaemonServer<R: Read, W: Write> {
     running: Cell<bool>,
     read: BufReader<R>,
     write: W,
-    hid: RefCell<Vec<Ec<AccessHid>>>,
+    hid: RefCell<Vec<Ec<Box<dyn Access>>>>,
     #[cfg(target_os = "linux")]
-    lpc: RefCell<Vec<Ec<AccessLpcLinux>>>,
+    lpc: RefCell<Vec<Ec<Box<dyn Access>>>>,
 }
 
 impl DaemonServer<io::Stdin, io::Stdout> {
@@ -36,7 +36,7 @@ impl<R: Read, W: Write> DaemonServer<R, W> {
             Ok(access) => match unsafe { Ec::new(access) } {
                 Ok(ec) => {
                     eprintln!("Adding LPC EC");
-                    lpc.push(ec);
+                    lpc.push(ec.into_dyn());
                 }
                 Err(err) => {
                     eprintln!("Failed to probe LPC EC: {:?}", err);
@@ -61,7 +61,7 @@ impl<R: Read, W: Write> DaemonServer<R, W> {
                                     Ok(access) => match unsafe { Ec::new(access) } {
                                         Ok(ec) => {
                                             eprintln!("Adding USB HID EC at {:?}", info.path());
-                                            hid.push(ec);
+                                            hid.push(ec.into_dyn());
                                         }
                                         Err(err) => {
                                             eprintln!(
@@ -128,6 +128,24 @@ impl<R: Read, W: Write> DaemonServer<R, W> {
 
         Ok(())
     }
+
+    fn board(&self, mut board: usize) -> Result<RefMut<Ec<Box<dyn Access>>>, String> {
+        #[cfg(target_os = "linux")]
+        {
+            let mut lpc = self.lpc.borrow_mut();
+            if lpc.get_mut(board).is_some() {
+                return Ok(RefMut::map(lpc, |x| x.get_mut(board).unwrap()));
+            }
+            board -= lpc.len();
+        }
+
+        let mut hid = self.hid.borrow_mut();
+        if hid.get_mut(board).is_some() {
+            return Ok(RefMut::map(hid, |x| x.get_mut(board).unwrap()));
+        }
+
+        Err("failed to find board".to_string())
+    }
 }
 
 impl<R: Read, W: Write> Daemon for DaemonServer<R, W> {
@@ -154,150 +172,53 @@ impl<R: Read, W: Write> Daemon for DaemonServer<R, W> {
         Ok(boards)
     }
 
-    fn keymap_get(
-        &self,
-        mut board: usize,
-        layer: u8,
-        output: u8,
-        input: u8,
-    ) -> Result<u16, String> {
-        #[cfg(target_os = "linux")]
-        {
-            let mut lpc = self.lpc.borrow_mut();
-            if let Some(ref mut ec) = lpc.get_mut(board) {
-                return unsafe { ec.keymap_get(layer, output, input).map_err(err_str) };
-            }
-            board -= lpc.len();
-        }
-
-        if let Some(ref mut ec) = self.hid.borrow_mut().get_mut(board) {
-            unsafe { ec.keymap_get(layer, output, input).map_err(err_str) }
-        } else {
-            Err("failed to find board".to_string())
-        }
+    fn keymap_get(&self, board: usize, layer: u8, output: u8, input: u8) -> Result<u16, String> {
+        let mut ec = self.board(board)?;
+        unsafe { ec.keymap_get(layer, output, input).map_err(err_str) }
     }
 
     fn keymap_set(
         &self,
-        mut board: usize,
+        board: usize,
         layer: u8,
         output: u8,
         input: u8,
         value: u16,
     ) -> Result<(), String> {
-        #[cfg(target_os = "linux")]
-        {
-            let mut lpc = self.lpc.borrow_mut();
-            if let Some(ref mut ec) = lpc.get_mut(board) {
-                return unsafe { ec.keymap_set(layer, output, input, value).map_err(err_str) };
-            }
-            board -= lpc.len();
-        }
+        let mut ec = self.board(board)?;
+        unsafe { ec.keymap_set(layer, output, input, value).map_err(err_str) }
+    }
 
-        if let Some(ref mut ec) = self.hid.borrow_mut().get_mut(board) {
-            unsafe { ec.keymap_set(layer, output, input, value).map_err(err_str) }
-        } else {
-            Err("failed to find board".to_string())
+    fn color(&self, board: usize) -> Result<Rgb, String> {
+        let mut ec = self.board(board)?;
+        unsafe {
+            ec.led_get_color(0xFF)
+                .map(|x| Rgb::new(x.0, x.1, x.2))
+                .map_err(err_str)
         }
     }
 
-    fn color(&self, mut board: usize) -> Result<Rgb, String> {
-        #[cfg(target_os = "linux")]
-        {
-            let mut lpc = self.lpc.borrow_mut();
-            if let Some(ref mut ec) = lpc.get_mut(board) {
-                return unsafe {
-                    ec.led_get_color(0xFF)
-                        .map(|x| Rgb::new(x.0, x.1, x.2))
-                        .map_err(err_str)
-                };
-            }
-            board -= lpc.len();
-        }
-
-        if let Some(ref mut ec) = self.hid.borrow_mut().get_mut(board) {
-            unsafe {
-                ec.led_get_color(0xFF)
-                    .map(|x| Rgb::new(x.0, x.1, x.2))
-                    .map_err(err_str)
-            }
-        } else {
-            Err("failed to find board".to_string())
+    fn set_color(&self, board: usize, color: Rgb) -> Result<(), String> {
+        let mut ec = self.board(board)?;
+        unsafe {
+            ec.led_set_color(0xFF, color.r, color.g, color.b)
+                .map_err(err_str)
         }
     }
 
-    fn set_color(&self, mut board: usize, color: Rgb) -> Result<(), String> {
-        #[cfg(target_os = "linux")]
-        {
-            let mut lpc = self.lpc.borrow_mut();
-            if let Some(ref mut ec) = lpc.get_mut(board) {
-                return unsafe {
-                    ec.led_set_color(0xFF, color.r, color.g, color.b)
-                        .map_err(err_str)
-                };
-            }
-            board -= lpc.len();
-        }
-
-        if let Some(ref mut ec) = self.hid.borrow_mut().get_mut(board) {
-            unsafe {
-                ec.led_set_color(0xFF, color.r, color.g, color.b)
-                    .map_err(err_str)
-            }
-        } else {
-            Err("failed to find board".to_string())
-        }
+    fn max_brightness(&self, board: usize) -> Result<i32, String> {
+        let mut ec = self.board(board)?;
+        unsafe { ec.led_get_value(0xFF).map(|x| x.1 as i32).map_err(err_str) }
     }
 
-    fn max_brightness(&self, mut board: usize) -> Result<i32, String> {
-        #[cfg(target_os = "linux")]
-        {
-            let mut lpc = self.lpc.borrow_mut();
-            if let Some(ref mut ec) = lpc.get_mut(board) {
-                return unsafe { ec.led_get_value(0xFF).map(|x| x.1 as i32).map_err(err_str) };
-            }
-            board -= lpc.len();
-        }
-
-        if let Some(ref mut ec) = self.hid.borrow_mut().get_mut(board) {
-            unsafe { ec.led_get_value(0xFF).map(|x| x.1 as i32).map_err(err_str) }
-        } else {
-            Err("failed to find board".to_string())
-        }
+    fn brightness(&self, board: usize) -> Result<i32, String> {
+        let mut ec = self.board(board)?;
+        unsafe { ec.led_get_value(0xFF).map(|x| x.0 as i32).map_err(err_str) }
     }
 
-    fn brightness(&self, mut board: usize) -> Result<i32, String> {
-        #[cfg(target_os = "linux")]
-        {
-            let mut lpc = self.lpc.borrow_mut();
-            if let Some(ref mut ec) = lpc.get_mut(board) {
-                return unsafe { ec.led_get_value(0xFF).map(|x| x.0 as i32).map_err(err_str) };
-            }
-            board -= lpc.len();
-        }
-
-        if let Some(ref mut ec) = self.hid.borrow_mut().get_mut(board) {
-            unsafe { ec.led_get_value(0xFF).map(|x| x.0 as i32).map_err(err_str) }
-        } else {
-            Err("failed to find board".to_string())
-        }
-    }
-
-    fn set_brightness(&self, mut board: usize, brightness: i32) -> Result<(), String> {
-        #[cfg(target_os = "linux")]
-        {
-            let mut lpc = self.lpc.borrow_mut();
-            if let Some(ref mut ec) = lpc.get_mut(board) {
-                return unsafe { ec.led_set_value(0xFF, brightness as u8).map_err(err_str) };
-            }
-            board -= lpc.len();
-        }
-
-        if let Some(ref mut ec) = self.hid.borrow_mut().get_mut(board) {
-            unsafe { ec.led_set_value(0xFF, brightness as u8).map_err(err_str) }
-        } else {
-            Err("failed to find board".to_string())
-        }
+    fn set_brightness(&self, board: usize, brightness: i32) -> Result<(), String> {
+        let mut ec = self.board(board)?;
+        unsafe { ec.led_set_value(0xFF, brightness as u8).map_err(err_str) }
     }
 
     fn exit(&self) -> Result<(), String> {
