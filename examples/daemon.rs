6782d643a19119cc2d08fab782167a8824df6d70
Ian Douglas Scott <idscott@system76.com>
2021-04-06 08:29:14 -0700

    fix: Make examples compile

--- examples/daemon.rs
+++ examples/daemon.rs
@@ -1,38 +1,32 @@
-use std::{env, io};
-use system76_keyboard_configurator::backend::{Daemon, DaemonClient, DaemonServer};
-
-fn daemon_server() -> Result<DaemonServer<io::Stdin, io::Stdout>, String> {
-    DaemonServer::new(io::stdin(), io::stdout())
-}
+use std::env;
+use system76_keyboard_configurator::backend::{run_daemon, Backend};
 
 #[cfg(target_os = "linux")]
-fn with_daemon<F: Fn(Box<dyn Daemon>)>(f: F) {
+fn with_daemon<F: Fn(Backend)>(f: F) {
     if unsafe { libc::geteuid() == 0 } {
         eprintln!("Already running as root");
-        let server = daemon_server().expect("Failed to create server");
-        f(Box::new(server));
+        let server = Backend::new().expect("Failed to create server");
+        f(server);
         return;
     }
 
-    f(Box::new(DaemonClient::new_pkexec()));
+    f(Backend::new_pkexec().unwrap());
 }
 
 #[cfg(not(target_os = "linux"))]
 fn with_daemon<F: Fn(Box<dyn Daemon>)>(f: F) {
-    let server = daemon_server().expect("Failed to create server");
-    f(Box::new(server));
+    let server = Backend::new().expect("Failed to create server");
+    f(server);
 }
 
 fn main() {
     for arg in env::args().skip(1) {
         if arg.as_str() == "--daemon" {
-            let server = daemon_server().expect("Failed to create server");
-            server.run().expect("Failed to run server");
-            return;
+            run_daemon()
         }
     }
 
-    with_daemon(|daemon| {
-        println!("boards: {:?}", daemon.boards());
+    with_daemon(|_daemon| {
+        // println!("boards: {:?}", daemon.boards());
     });
 }
