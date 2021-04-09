bcc20ec952722d71f5dbaeca00a8c779efd63f1b
Ian Douglas Scott <idscott@system76.com>
2021-04-09 09:18:09 -0700

    improv: Use zbus for `s76power` backend
    Better native Rust API, which also implements `Send`.

--- backend/src/daemon/s76power.rs
+++ backend/src/daemon/s76power.rs
@@ -2,75 +2,44 @@
 // Need to watch properties of each object?
 // TODO: Hotplug detection support
 
-use gio::prelude::*;
-use glib::variant::{FromVariant, ToVariant};
-use std::{cell::Cell, iter::Iterator};
+use std::iter::Iterator;
+use zbus::{dbus_proxy, fdo::ObjectManagerProxy, Connection};
 
 use super::{err_str, BoardId, Daemon, Matrix};
 use crate::Rgb;
 
 const DBUS_NAME: &str = "com.system76.PowerDaemon";
-const DBUS_KEYBOARD_IFACE: &str = "com.system76.PowerDaemon.Keyboard";
+
+#[dbus_proxy(interface = "com.system76.PowerDaemon.Keyboard")]
+trait Keyboard {
+    #[dbus_proxy(property)]
+    fn brightness(&self) -> zbus::Result<i32>;
+    #[dbus_proxy(property)]
+    fn set_brightness(&self, value: i32) -> zbus::Result<()>;
+
+    #[dbus_proxy(property)]
+    fn color(&self) -> zbus::Result<String>;
+    #[dbus_proxy(property)]
+    fn set_color(&self, value: &str) -> zbus::Result<()>;
+
+    #[dbus_proxy(property)]
+    fn max_brightness(&self) -> zbus::Result<i32>;
+
+    #[dbus_proxy(property)]
+    fn name(&self) -> zbus::Result<String>;
+}
 
 struct Keyboard {
-    proxy: gio::DBusProxy,
-    properties_proxy: gio::DBusProxy,
-    brightness_set_cancellable: Cell<gio::Cancellable>,
-    color_set_cancellable: Cell<gio::Cancellable>,
+    proxy: KeyboardProxy<'static>,
 }
 
 impl Keyboard {
     fn new(path: &str) -> Result<Self, String> {
-        let proxy = gio::DBusProxy::new_for_bus_sync::<gio::Cancellable>(
-            gio::BusType::System,
-            gio::DBusProxyFlags::NONE,
-            None,
-            DBUS_NAME,
-            path,
-            DBUS_KEYBOARD_IFACE,
-            None,
-        )
-        .map_err(err_str)?;
-        let properties_proxy = gio::DBusProxy::new_for_bus_sync::<gio::Cancellable>(
-            gio::BusType::System,
-            gio::DBusProxyFlags::NONE,
-            None,
-            DBUS_NAME,
-            path,
-            "org.freedesktop.DBus.Properties",
-            None,
-        )
-        .map_err(err_str)?;
-        Ok(Self {
-            proxy,
-            properties_proxy,
-            brightness_set_cancellable: Cell::new(gio::Cancellable::new()),
-            color_set_cancellable: Cell::new(gio::Cancellable::new()),
-        })
-    }
-
-    fn prop<T: FromVariant>(&self, name: &'static str) -> Result<Option<T>, String> {
-        Ok(self.proxy.get_cached_property(name).and_then(|v| v.get()))
-    }
-
-    fn set_prop<T: ToVariant>(
-        &self,
-        name: &'static str,
-        value: T,
-        cancellable: &Cell<gio::Cancellable>,
-    ) -> Result<(), String> {
-        let new_cancellable = gio::Cancellable::new();
-        let args = (DBUS_KEYBOARD_IFACE, name, value.to_variant());
-        self.properties_proxy.call(
-            "Set",
-            Some(&args.to_variant()),
-            gio::DBusCallFlags::NONE,
-            60000,
-            Some(&new_cancellable),
-            |_| {},
-        );
-        cancellable.replace(new_cancellable).cancel();
-        Ok(())
+        let connection = Connection::new_system().map_err(err_str)?;
+        let proxy =
+            KeyboardProxy::new_for_owned(connection, DBUS_NAME.to_string(), path.to_string())
+                .map_err(err_str)?;
+        Ok(Self { proxy })
     }
 }
 
@@ -90,28 +59,13 @@ impl DaemonS76Power {
     pub fn new() -> Result<Self, String> {
         let mut boards = Vec::new();
 
-        let proxy = gio::DBusProxy::new_for_bus_sync::<gio::Cancellable>(
-            gio::BusType::System,
-            gio::DBusProxyFlags::NONE,
-            None,
-            DBUS_NAME,
-            "/com/system76/PowerDaemon",
-            "org.freedesktop.DBus.ObjectManager",
-            None,
-        )
-        .map_err(err_str)?;
-        let ret = proxy
-            .call_sync::<gio::Cancellable>(
-                "GetManagedObjects",
-                None,
-                gio::DBusCallFlags::NONE,
-                60000,
-                None,
-            )
-            .map_err(err_str)?;
+        let connection = Connection::new_system().map_err(err_str)?;
+        let proxy =
+            ObjectManagerProxy::new_for(&connection, DBUS_NAME, "/com/system76/PowerDaemon")
+                .map_err(err_str)?;
+        let objects = proxy.get_managed_objects().map_err(err_str)?;
 
-        for i in ret.get_child_value(0).iter() {
-            let path = i.get_child_value(0).get::<String>().unwrap();
+        for path in objects.keys() {
             if path.starts_with("/com/system76/PowerDaemon/keyboard") {
                 boards.push(Keyboard::new(&path)?);
             }
@@ -129,8 +83,9 @@ impl Daemon for DaemonS76Power {
     fn model(&self, board: BoardId) -> Result<String, String> {
         Ok(self
             .board(board)?
-            .prop::<String>("name")?
-            .unwrap_or_else(|| "".to_string()))
+            .proxy
+            .name()
+            .unwrap_or_else(|_| "".to_string()))
     }
 
     fn keymap_get(
@@ -162,43 +117,39 @@ impl Daemon for DaemonS76Power {
         if index != 0xFF {
             return Err(format!("Can't set color index {}", index));
         }
-        let color = self.board(board)?.prop::<String>("color")?;
-        Ok(color
-            .and_then(|c| Rgb::parse(&c))
-            .map_or((0, 0, 0), |rgb| (rgb.r, rgb.g, rgb.b)))
+        let color = self.board(board)?.proxy.color().map_err(err_str)?;
+        Ok(Rgb::parse(&color).map_or((0, 0, 0), |rgb| (rgb.r, rgb.g, rgb.b)))
     }
 
     fn set_color(&self, board: BoardId, index: u8, color: (u8, u8, u8)) -> Result<(), String> {
         if index != 0xFF {
             return Err(format!("Can't set color index {}", index));
         }
-        let board = self.board(board)?;
-        board.set_prop(
-            "color",
-            Rgb::new(color.0, color.1, color.2).to_string(),
-            &board.color_set_cancellable,
-        )?;
-        Ok(())
+        self.board(board)?
+            .proxy
+            .set_color(&Rgb::new(color.0, color.1, color.2).to_string())
+            .map_err(err_str)
     }
 
     fn max_brightness(&self, board: BoardId) -> Result<i32, String> {
-        Ok(self.board(board)?.prop("max_brightness")?.unwrap_or(100))
+        Ok(self.board(board)?.proxy.max_brightness().map_err(err_str)?)
     }
 
     fn brightness(&self, board: BoardId, index: u8) -> Result<i32, String> {
         if index != 0xFF {
             return Err(format!("Can't set brightness index {}", index));
         }
-        Ok(self.board(board)?.prop("brightness")?.unwrap_or(0))
+        Ok(self.board(board)?.proxy.brightness().map_err(err_str)?)
     }
 
     fn set_brightness(&self, board: BoardId, index: u8, brightness: i32) -> Result<(), String> {
         if index != 0xFF {
             return Err(format!("Can't set brightness index {}", index));
         }
-        let board = self.board(board)?;
-        board.set_prop("brightness", brightness, &board.brightness_set_cancellable)?;
-        Ok(())
+        self.board(board)?
+            .proxy
+            .set_brightness(brightness)
+            .map_err(err_str)
     }
 
     fn mode(&self, _board: BoardId, _layer: u8) -> Result<(u8, u8), String> {
