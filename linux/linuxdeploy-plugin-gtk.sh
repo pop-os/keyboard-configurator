69f53490e2dc1d6a9e2fa53619fc5607a75f00fe
Ian Douglas Scott <idscott@system76.com>
2021-05-05 14:37:47 -0700

    fix: Update linuxdeploy
    Should fix appimage running on Fedora Rawhide.

--- linux/linuxdeploy-plugin-gtk.sh
+++ linux/linuxdeploy-plugin-gtk.sh
@@ -25,6 +25,30 @@ copy_tree() {
     done
 }
 
+search_tool() {
+    local tool="$1"
+    local directory="$2"
+
+    if command -v "$tool"; then
+        return 0
+    fi
+
+    PATH_ARRAY=(
+        "/usr/lib/$(uname -m)-linux-gnu/$directory/$tool"
+        "/usr/lib/$directory/$tool"
+        "/usr/bin/$tool"
+        "/usr/bin/$tool-64"
+        "/usr/bin/$tool-32"
+    )
+
+    for path in "${PATH_ARRAY[@]}"; do
+        if [ -x "$path" ]; then
+            echo "$path"
+            return 0
+        fi
+    done
+}
+
 APPDIR=
 
 while [ "$1" != "" ]; do
@@ -67,6 +91,16 @@ else
     exit 1
 fi
 
+if ! command -v patchelf &>/dev/null && ! type patchelf &>/dev/null; then
+    echo -e "$0: patchelf not found.\nInstall patchelf then re-run the plugin."
+    exit 1
+fi
+
+if [ -z "$LINUXDEPLOY" ]; then
+    echo -e "$0: LINUXDEPLOY environment variable is not set.\nDownload a suitable linuxdeploy AppImage, set the environment variable and re-run the plugin."
+    exit 1
+fi
+
 echo "Installing AppRun hook"
 HOOKSDIR="$APPDIR/apprun-hooks"
 HOOKFILE="$HOOKSDIR/linuxdeploy-plugin-gtk.sh"
@@ -74,11 +108,15 @@ mkdir -p "$HOOKSDIR"
 cat > "$HOOKFILE" <<\EOF
 #! /bin/bash
 
+# gsettings get org.gnome.desktop.interface gtk-theme 2> /dev/null | grep -qi "dark" && GTK_THEME_VARIANT="dark" || GTK_THEME_VARIANT="light"
+# APPIMAGE_GTK_THEME="${APPIMAGE_GTK_THEME:-"Adwaita:$GTK_THEME_VARIANT"}" # Allow user to override theme (discouraged)
 CACHEDIR="$(mktemp --tmpdir --directory .AppRun.XXXXXXXX)"
 
 export APPDIR="${APPDIR:-"$(dirname "$(realpath "$0")")"}" # Workaround to run extracted AppImage
 export GTK_DATA_PREFIX="$APPDIR"
+# export GTK_THEME="$APPIMAGE_GTK_THEME" # Custom themes are broken
 export GDK_BACKEND=x11 # Crash with Wayland backend on Wayland
+export XDG_DATA_DIRS="$APPDIR/usr/share:/usr/share:$XDG_DATA_DIRS" # g_get_system_data_dirs() from GLib
 EOF
 
 echo "Installing GLib schemas"
@@ -94,7 +132,9 @@ echo "Installing GTK 3.0 modules"
 gtk3_exec_prefix="$("$PKG_CONFIG" --variable=exec_prefix gtk+-3.0)"
 gtk3_libdir="$("$PKG_CONFIG" --variable=libdir gtk+-3.0)/gtk-3.0"
 gtk3_immodulesdir="$gtk3_libdir/$("$PKG_CONFIG" --variable=gtk_binary_version gtk+-3.0)/immodules"
+gtk3_printbackendsdir="$gtk3_libdir/$("$PKG_CONFIG" --variable=gtk_binary_version gtk+-3.0)/printbackends"
 gtk3_immodules_cache_file="$(dirname "$gtk3_immodulesdir")/immodules.cache"
+gtk3_immodules_query="$(search_tool "gtk-query-immodules-3.0" "libgtk-3-0")"
 copy_tree "$gtk3_libdir" "$APPDIR/"
 cat >> "$HOOKFILE" <<EOF
 export GTK_EXE_PREFIX="\$APPDIR/$gtk3_exec_prefix"
@@ -103,30 +143,74 @@ export GTK_IM_MODULE_DIR="\$APPDIR/$gtk3_immodulesdir"
 export GTK_IM_MODULE_FILE="\$CACHEDIR/immodules.cache"
 sed "s|$gtk3_libdir|\$APPDIR/$gtk3_libdir|g" "\$APPDIR/$gtk3_immodules_cache_file" > "\$GTK_IM_MODULE_FILE"
 EOF
+if [ -x "$gtk3_immodules_query" ]; then
+    echo "Updating immodules cache in $APPDIR/$gtk3_immodules_cache_file"
+    "$gtk3_immodules_query" > "$APPDIR/$gtk3_immodules_cache_file"
+else
+    echo "WARNING: gtk-query-immodules-3.0 not found"
+fi
+if [ ! -f "$APPDIR/$gtk3_immodules_cache_file" ]; then
+    echo "WARNING: immodules.cache file is missing"
+fi
 
 echo "Installing GDK PixBufs"
 gdk_libdir="$("$PKG_CONFIG" --variable=libdir gdk-pixbuf-2.0)"
 gdk_pixbuf_binarydir="$("$PKG_CONFIG" --variable=gdk_pixbuf_binarydir gdk-pixbuf-2.0)"
 gdk_pixbuf_cache_file="$("$PKG_CONFIG" --variable=gdk_pixbuf_cache_file gdk-pixbuf-2.0)"
 gdk_pixbuf_moduledir="$("$PKG_CONFIG" --variable=gdk_pixbuf_moduledir gdk-pixbuf-2.0)"
+# Note: gdk_pixbuf_query_loaders variable is not defined on some systems
+gdk_pixbuf_query="$(search_tool "gdk-pixbuf-query-loaders" "gdk-pixbuf-2.0")"
 copy_tree "$gdk_pixbuf_binarydir" "$APPDIR/"
 cat >> "$HOOKFILE" <<EOF
 export GDK_PIXBUF_MODULEDIR="\$APPDIR/$gdk_pixbuf_moduledir"
 export GDK_PIXBUF_MODULE_FILE="\$CACHEDIR/loaders.cache"
-export LD_LIBRARY_PATH="\$GDK_PIXBUF_MODULEDIR:\$LD_LIBRARY_PATH"
 sed "s|$gdk_pixbuf_moduledir|\$APPDIR/$gdk_pixbuf_moduledir|g" "\$APPDIR/$gdk_pixbuf_cache_file" > "\$GDK_PIXBUF_MODULE_FILE"
 EOF
+if [ -x "$gdk_pixbuf_query" ]; then
+    echo "Updating pixbuf cache in $APPDIR/$gdk_pixbuf_cache_file"
+    "$gdk_pixbuf_query" > "$APPDIR/$gdk_pixbuf_cache_file"
+else
+    echo "WARNING: gdk-pixbuf-query-loaders not found"
+fi
+if [ ! -f "$APPDIR/$gdk_pixbuf_cache_file" ]; then
+    echo "WARNING: loaders.cache file is missing"
+fi
 
 echo "Copying more libraries"
 gobject_libdir="$("$PKG_CONFIG" --variable=libdir gobject-2.0)"
 gio_libdir="$("$PKG_CONFIG" --variable=libdir gio-2.0)"
 librsvg_libdir="$("$PKG_CONFIG" --variable=libdir librsvg-2.0)"
-cp $verbose \
-    "$gdk_libdir/"libgdk_pixbuf*.so* \
-    "$gobject_libdir/"libgobject*.so* \
-    "$gio_libdir/"libgio*.so* \
-    "$librsvg_libdir/"librsvg*.so* \
-    "$APPDIR/usr/lib/"
-cat >> "$HOOKFILE" <<EOF
-export LD_LIBRARY_PATH="\$APPDIR/usr/lib:\$LD_LIBRARY_PATH"
-EOF
+pango_libdir="$("$PKG_CONFIG" --variable=libdir pango)"
+pangocairo_libdir="$("$PKG_CONFIG" --variable=libdir pangocairo)"
+pangoft2_libdir="$("$PKG_CONFIG" --variable=libdir pangoft2)"
+FIND_ARRAY=(
+    "$gdk_libdir"     "libgdk_pixbuf-*.so*"
+    "$gobject_libdir" "libgobject-*.so*"
+    "$gio_libdir"     "libgio-*.so*"
+    "$librsvg_libdir" "librsvg-*.so*"
+    "$pango_libdir"      "libpango-*.so*"
+    "$pangocairo_libdir" "libpangocairo-*.so*"
+    "$pangoft2_libdir"   "libpangoft2-*.so*"
+)
+LIBRARIES=()
+for (( i=0; i<${#FIND_ARRAY[@]}; i+=2 )); do
+    directory=${FIND_ARRAY[i]}
+    library=${FIND_ARRAY[i+1]}
+    while IFS= read -r -d '' file; do
+        LIBRARIES+=( "--library=$file" )
+    done < <(find "$directory" \( -type l -o -type f \) -name "$library" -print0)
+done
+"$LINUXDEPLOY" --appdir="$APPDIR" "${LIBRARIES[@]}"
+
+echo "Manually setting rpath for GTK modules"
+PATCH_ARRAY=(
+    "$gtk3_immodulesdir"
+    "$gtk3_printbackendsdir"
+    "$gdk_pixbuf_moduledir"
+)
+for directory in "${PATCH_ARRAY[@]}"; do
+    while IFS= read -r -d '' file; do
+        # shellcheck disable=SC2016
+        patchelf --set-rpath '$ORIGIN/../../../..' "$APPDIR/$file"
+    done < <(find "$directory" -name '*.so' -print0)
+done
