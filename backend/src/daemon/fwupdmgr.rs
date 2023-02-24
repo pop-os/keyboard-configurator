e7b50468c7c162d4bd4802581ca2c8a1ac5210dd
13r0ck <brock@szu.email>
2023-02-24 12:18:41 -0700

    Use `fwupdmgr get-update --json`
    Rather than trying to parse for a specific device,
    `get-updates --json` will only include the name of a device
    if there is a firmware update for it. If the output contains
    `Configurable Keyboard` we can assume for production's sake,
    that that is a `Launch * Configurable Keyboard`, and that it isn't
    updated.

--- backend/src/daemon/fwupdmgr.rs
+++ backend/src/daemon/fwupdmgr.rs
@@ -4,48 +4,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "PascalCase")]
-struct Devices {
-    pub devices: Vec<Device>,
-}
-
-impl Devices {
-    pub fn get_launch(self) -> Result<Device, Error> {
-        self.devices
-            .into_iter()
-            .find(|device| {
-                device
-                    .name
-                    .as_ref()
-                    .unwrap_or(&"".to_string())
-                    .contains("Launch")
-                    && device.vendor.as_ref().unwrap_or(&"".to_string()) == "System76"
-            })
-            .ok_or(Error::NoLaunch)
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "PascalCase")]
-struct Device {
-    name: Option<String>,
-    vendor: Option<String>,
-    created: usize,
-    releases: Option<Vec<Release>>,
-}
-
-impl Device {
-    pub fn is_newer_release(self) -> Result<bool, Error> {
-        Ok(self
-            .releases
-            .as_ref()
-            .ok_or(Error::NoRelease)?
-            .into_iter()
-            .any(|release| release.created > self.created))
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "PascalCase")]
 struct Release {
     created: usize,
 }
@@ -53,24 +11,14 @@ struct Release {
 #[derive(Copy, Clone, Debug)]
 pub enum Error {
     Fwupdmgr,
-    Json,
-    NoLaunch,
-    NoRelease,
+    Utf8,
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             Error::Fwupdmgr => write!(f, "Failed running the fwupdmgr command"),
-            Error::Json => write!(f, "Return data from fwupd was not parsed correctly"),
-            Error::NoLaunch => write!(
-                f,
-                "Unable to find a device with appstream id `com.system76.launch*` via fwupdmgr"
-            ),
-            Error::NoRelease => write!(
-                f,
-                "Unable to find a current release for the connected launch."
-            ),
+            Error::Utf8 => write!(f, "Return data from fwupd was not parsed correctly"),
         }
     }
 }
@@ -83,32 +31,16 @@ impl From<std::io::Error> for Error {
 
 impl From<std::str::Utf8Error> for Error {
     fn from(_e: std::str::Utf8Error) -> Self {
-        Error::Json
-    }
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(_e: serde_json::Error) -> Self {
-        Error::Json
-    }
-}
-
-impl From<std::num::ParseIntError> for Error {
-    fn from(_e: std::num::ParseIntError) -> Self {
-        Error::Json
+        Error::Utf8
     }
 }
 
 pub fn is_launch_updated() -> Result<bool, Error> {
-    // Get all fwupd devices
     let stdout = Command::new("fwupdmgr")
-        .args(["get-devices", "--json"])
+        .args(["get-updates", "--json"])
         .output()?
         .stdout;
-    info!("before json");
-    let json = std::str::from_utf8(&stdout)?;
-    info!("before devices");
-    let devices: Devices = serde_json::from_str(json).unwrap();
+    let stdout = std::str::from_utf8(&stdout)?;
 
-    devices.get_launch()?.is_newer_release()
+    Ok(!stdout.contains("Configurable Keyboard"))
 }
