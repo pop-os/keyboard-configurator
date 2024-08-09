2d9f932b6ae48710a78618e260c9f36389a8a572
Ian Douglas Scott <idscott@system76.com>
2024-08-08 20:26:20 -0700

    windows: Update path for new version of `CodeSignTool`
    The format of the `CodeSignTool` zip archive seems to have changed.

--- windows/build.py
+++ windows/build.py
@@ -212,9 +212,9 @@ if args.sign:
         "-password=" + os.environ["SSL_COM_PASSWORD"],
         "-totp_secret=" + os.environ["SSL_COM_TOTP_SECRET"],
         "-program_name=System76 Keyboard Configurator",
-        "-input_file_path=../../../keyboard-configurator.msi",
-        "-output_dir_path=../../",
-    ], cwd="sign/CodeSignTool/CodeSignTool-v1.2.7-windows")
+        "-input_file_path=../../keyboard-configurator.msi",
+        "-output_dir_path=../",
+    ], cwd="sign/CodeSignTool")
 
     # Update MSI
     os.remove("keyboard-configurator.msi")
