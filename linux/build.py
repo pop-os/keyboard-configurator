2206526edad70003ff04165f63e8c3dc11fb8109
Aaron Honeycutt <aaronhoneycutt@proton.me>
2024-12-06 10:16:25 -0700

    disable x86 for testing Arm

--- linux/build.py
+++ linux/build.py
@@ -40,48 +40,48 @@ subprocess.check_call(cmd)
 # Copy executable
 subprocess.check_call([f"strip", '-o', "system76-keyboard-configurator", f"{TARGET_DIR}/system76-keyboard-configurator"])
 
-# x86_64 Section
+# # x86_64 Section
 
-## Download linuxdeploy
-LINUXDEPLOY = f"linuxdeploy-{ARCH_x86}.AppImage"
-LINUXDEPLOY_URL = f"https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/{LINUXDEPLOY}"
-if not os.path.exists(LINUXDEPLOY):
-    with urlopen(LINUXDEPLOY_URL) as u:
-        with open(LINUXDEPLOY, 'wb') as f:
-            f.write(u.read())
-    os.chmod(LINUXDEPLOY, os.stat(LINUXDEPLOY).st_mode | 0o111)
+# ## Download linuxdeploy
+# LINUXDEPLOY_x86 = f"linuxdeploy-{ARCH_x86}.AppImage"
+# LINUXDEPLOY_x86_URL = f"https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/{LINUXDEPLOY_x86}"
+# if not os.path.exists(LINUXDEPLOY_x86):
+#     with urlopen(LINUXDEPLOY_x86_URL) as u:
+#         with open(LINUXDEPLOY_x86, 'wb') as f:
+#             f.write(u.read())
+#     os.chmod(LINUXDEPLOY_x86, os.stat(LINUXDEPLOY_x86).st_mode | 0o111)
 
-## Copy appdata
-os.makedirs(f"{PKG}.AppDir/usr/share/metainfo")
-shutil.copy("com.system76.keyboardconfigurator.appdata.xml", f"{PKG}.AppDir/usr/share/metainfo")
+# ## Copy appdata
+# os.makedirs(f"{PKG}.AppDir/usr/share/metainfo")
+# shutil.copy("com.system76.keyboardconfigurator.appdata.xml", f"{PKG}.AppDir/usr/share/metainfo")
 
-## Build appimage
-subprocess.check_call([f"./{LINUXDEPLOY}",
-                       f"--appdir={PKG}.AppDir",
-                       f"--executable=system76-keyboard-configurator",
-                       f"--desktop-file={APPID}.desktop",
-                       f"--icon-file={ICON}",
-                        "--plugin", "gtk",
-                        "--output", "appimage"])
-shutil.move(f"System76_Keyboard_Configurator-{ARCH_x86}.AppImage", f"{PKG}-{ARCH_x86}.AppImage")
+# ## Build appimage
+# subprocess.check_call([f"./{LINUXDEPLOY_x86}",
+#                        f"--appdir={PKG}.AppDir",
+#                        f"--executable=system76-keyboard-configurator",
+#                        f"--desktop-file={APPID}.desktop",
+#                        f"--icon-file={ICON}",
+#                         "--plugin", "gtk",
+#                         "--output", "appimage"])
+# shutil.move(f"System76_Keyboard_Configurator-{ARCH_x86}.AppImage", f"{PKG}-{ARCH_x86}.AppImage")
 
 # arm64 Section
 
 ## Download linuxdeploy
-LINUXDEPLOY = f"linuxdeploy-{ARCH_Arm}.AppImage"
-LINUXDEPLOY_URL = f"https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/{LINUXDEPLOY}"
-if not os.path.exists(LINUXDEPLOY):
-    with urlopen(LINUXDEPLOY_URL) as u:
-        with open(LINUXDEPLOY, 'wb') as f:
+LINUXDEPLOY_Arm = f"linuxdeploy-{ARCH_Arm}.AppImage"
+LINUXDEPLOY_Arm_URL = f"https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/{LINUXDEPLOY_Arm}"
+if not os.path.exists(LINUXDEPLOY_Arm):
+    with urlopen(LINUXDEPLOY_Arm_URL) as u:
+        with open(LINUXDEPLOY_Arm, 'wb') as f:
             f.write(u.read())
-    os.chmod(LINUXDEPLOY, os.stat(LINUXDEPLOY).st_mode | 0o111)
+    os.chmod(LINUXDEPLOY_Arm, os.stat(LINUXDEPLOY_Arm).st_mode | 0o111)
 
 ## Copy appdata
 os.makedirs(f"{PKG}.AppDir/usr/share/metainfo")
 shutil.copy("com.system76.keyboardconfigurator.appdata.xml", f"{PKG}.AppDir/usr/share/metainfo")
 
 ## Build appimage
-subprocess.check_call([f"./{LINUXDEPLOY}",
+subprocess.check_call([f"./{LINUXDEPLOY_Arm}",
                        f"--appdir={PKG}.AppDir",
                        f"--executable=system76-keyboard-configurator",
                        f"--desktop-file={APPID}.desktop",
