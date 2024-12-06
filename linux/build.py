33a18e11e90fcd87ccee8edaa0df485595e79687
Aaron Honeycutt <aaronhoneycutt@proton.me>
2024-12-06 10:51:39 -0700

    update after testing

--- linux/build.py
+++ linux/build.py
@@ -42,28 +42,28 @@ subprocess.check_call([f"strip", '-o', "system76-keyboard-configurator", f"{TARG
 
 # # x86_64 Section
 
-# ## Download linuxdeploy
-# LINUXDEPLOY_x86 = f"linuxdeploy-{ARCH_x86}.AppImage"
-# LINUXDEPLOY_x86_URL = f"https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/{LINUXDEPLOY_x86}"
-# if not os.path.exists(LINUXDEPLOY_x86):
-#     with urlopen(LINUXDEPLOY_x86_URL) as u:
-#         with open(LINUXDEPLOY_x86, 'wb') as f:
-#             f.write(u.read())
-#     os.chmod(LINUXDEPLOY_x86, os.stat(LINUXDEPLOY_x86).st_mode | 0o111)
+## Download linuxdeploy
+LINUXDEPLOY_x86 = f"linuxdeploy-{ARCH_x86}.AppImage"
+LINUXDEPLOY_x86_URL = f"https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/{LINUXDEPLOY_x86}"
+if not os.path.exists(LINUXDEPLOY_x86):
+    with urlopen(LINUXDEPLOY_x86_URL) as u:
+        with open(LINUXDEPLOY_x86, 'wb') as f:
+            f.write(u.read())
+    os.chmod(LINUXDEPLOY_x86, os.stat(LINUXDEPLOY_x86).st_mode | 0o111)
 
-# ## Copy appdata
-# os.makedirs(f"{PKG}.AppDir/usr/share/metainfo")
-# shutil.copy("com.system76.keyboardconfigurator.appdata.xml", f"{PKG}.AppDir/usr/share/metainfo")
+## Copy appdata
+os.makedirs(f"{PKG}.AppDir/usr/share/metainfo")
+shutil.copy("com.system76.keyboardconfigurator.appdata.xml", f"{PKG}.AppDir/usr/share/metainfo")
 
-# ## Build appimage
-# subprocess.check_call([f"./{LINUXDEPLOY_x86}",
-#                        f"--appdir={PKG}.AppDir",
-#                        f"--executable=system76-keyboard-configurator",
-#                        f"--desktop-file={APPID}.desktop",
-#                        f"--icon-file={ICON}",
-#                         "--plugin", "gtk",
-#                         "--output", "appimage"])
-# shutil.move(f"System76_Keyboard_Configurator-{ARCH_x86}.AppImage", f"{PKG}-{ARCH_x86}.AppImage")
+## Build appimage
+subprocess.check_call([f"./{LINUXDEPLOY_x86}",
+                       f"--appdir={PKG}.AppDir",
+                       f"--executable=system76-keyboard-configurator",
+                       f"--desktop-file={APPID}.desktop",
+                       f"--icon-file={ICON}",
+                        "--plugin", "gtk",
+                        "--output", "appimage"])
+shutil.move(f"System76_Keyboard_Configurator-{ARCH_x86}.AppImage", f"{PKG}-{ARCH_x86}.AppImage")
 
 # arm64 Section
 
@@ -77,8 +77,8 @@ if not os.path.exists(LINUXDEPLOY_Arm):
     os.chmod(LINUXDEPLOY_Arm, os.stat(LINUXDEPLOY_Arm).st_mode | 0o111)
 
 ## Copy appdata
-os.makedirs(f"{PKG}.AppDir/usr/share/metainfo")
-shutil.copy("com.system76.keyboardconfigurator.appdata.xml", f"{PKG}.AppDir/usr/share/metainfo")
+#os.makedirs(f"{PKG}.AppDir/usr/share/metainfo")
+#shutil.copy("com.system76.keyboardconfigurator.appdata.xml", f"{PKG}.AppDir/usr/share/metainfo")
 
 ## Build appimage
 subprocess.check_call([f"./{LINUXDEPLOY_Arm}",
